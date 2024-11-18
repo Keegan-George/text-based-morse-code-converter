0720272e599aa4e5d393b4595f5f3cf61c1759fb
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2024-11-18 15:42:35 -0500

    Modify encyyption code to use .get() to support multiple word phrases

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from morse_code import morse_code_map
 
 user_input = input("Enter the phrase to be encoded in Morse code:").upper()
 
-encrypted = [morse_code_map[character] for word in user_input for character in word]
+encrypted = [
+    morse_code_map.get(character, character)
+    for word in user_input
+    for character in word
+]
 
 print(f"Your converted phrase in morse code is: {''.join(encrypted)}")
