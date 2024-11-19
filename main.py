d8958416caf917446c54a126081373ca57659ec2
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2024-11-19 11:39:35 -0500

    Refactor code to have morse encoding in dedication function

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ A text-based Morse code converter that prompts the user to enter a phrase and th
 Morse, as per the International Morse code.
 """
 
-
 from morse_code import morse_code_map
+
+
+def morse_encoder(phrase: str) -> str:
+    return "       ".join(
+        [
+            " ".join([morse_code_map.get(character, "") for character in word])
+            for word in phrase
+        ]
+    )
+
 
 user_input = input("Enter the phrase to be encoded in Morse Code:").upper().split()
 
-morse_encrypted = "       ".join(
-    [
-        " ".join([morse_code_map.get(character, "") for character in word])
-        for word in user_input
-    ]
-)
-
-print(f"Your converted phrase in Morse Code is: {morse_encrypted}")
+print(f"Your converted phrase in Morse Code is: {morse_encoder(user_input)}")
