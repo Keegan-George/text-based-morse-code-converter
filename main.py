183d4288f65507bc505fbc951e07cea3ad3e40ae
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-12 10:06:05 -0400

    Include variable for the number of spaces between words and removed unnecessary lists. Other minor formatting and code clean up

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """
-A text-based Morse code converter that prompts the user to enter a phrase and then returns a new string, Morses encoded, 
+A text-based Morse code converter that prompts the user to enter a phrase and then returns a new string, Morses encoded,
 as per the International Morse Code.
 """
 
 from morse_code_map import morse_code_map
 
+SPACES_BETWEEN_WORDS = " " * 7
 
-def morse_encoder(phrase: str) -> str:
-    """Take a string and return its Morse Code equivalent"""
-    return "       ".join(
-        [
-            " ".join([morse_code_map.get(character, "") for character in word])
-            for word in phrase
-        ]
+def morse_encoder(phrase: list[str]) -> str:
+    """Convert a list of strings into their Morse Code equivalent"""
+
+    return SPACES_BETWEEN_WORDS.join(
+        " ".join(morse_code_map.get(character, "") for character in word)
+        for word in phrase
     )
 
 
